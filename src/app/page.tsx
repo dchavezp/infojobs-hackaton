88b9520ff2d9c7639c3fce264829f2558bdc026f
DEWITT SCOTT CHAVEZ PONCE <dschavezp@gmail.com>
2023-05-24 11:51:56 -0500

    updating main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import SignInButton from "@/components/sign-in-button";
 export default function Home() {
   return (
     <main className="w-screen h-screen flex items-center justify-center">
+      <h1>Sign With Infojobs</h1>
       <SignInButton />
     </main>
   );
